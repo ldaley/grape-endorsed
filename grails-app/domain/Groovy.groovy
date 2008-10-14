ab6f7ce0111e412e983ff7f4a3a6c1ae05daa1ef
Luke Daley <ld@ldaley.com>
2008-10-14 23:12:25 +1000

    • Add unique constraint to groovy.tag

--- grails-app/domain/Groovy.groovy
+++ grails-app/domain/Groovy.groovy
@@ -1,8 +1,12 @@
 class Groovy implements Comparable {
+    
+    static constraints = {
+        tag(unique: true, nullable: false, blank: false, matches: /^\d.*/)
+    }
+    
     static belongsTo = ModuleVersion
     static hasMany = [moduleVersions: ModuleVersion]
     String tag
-    Date released
     
     int compareTo(o) {
         this.tag <=> o.tag 
